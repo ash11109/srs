c0e86a13d62309a2356c9a7710387c189a9027ff
Himanshi <99028043+Himanshi031@users.noreply.github.com>
2024-07-27 12:41:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Homepage';
 
 function App() {
+  
   return (
-    <div>
-        <span>start from here...</span>      
-    </div>
+      <div>
+        <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+        </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
-
 export default App;
