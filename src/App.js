677ee4da5b2889364d9472f43dd3d692e73de3cc
Himanshi <99028043+Himanshi031@users.noreply.github.com>
2024-08-03 08:05:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Homepage';
+import About from './components/About/Aboutpage';
+import Portfolio from './components/Portfolio/Portfoliopage';
+import Blog from './components/Blog/Blogpage';
+import Contact from './components/Contact/Contactpage';
 
 function App() {
   
@@ -9,6 +13,11 @@ function App() {
         <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/portfolio" element={<Portfolio/>}/>
+          <Route exact path="/blog" element={<Blog/>}/>
+          <Route exact path="/contact" element={<Contact/>}/>
+          
         </Routes>
         </BrowserRouter>
       </div>
