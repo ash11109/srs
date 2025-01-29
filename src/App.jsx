db3270b99d67d265f382e3cca793d9c7d86cb5e6
ash11109 <ayushcool1275@gmail.com>
2025-01-30 03:31:53 +0530

    bug solved for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import About from './pages/about';
+import About from './pages/About';
 import WebDevelopment from './pages/services/WebDevelopment';
 import AppDevelopment from './pages/services/AppDevelopment';
 import DigitalMarketing from './pages/services/DigitalMarketing';
