0496660929b52a0bea6d61335bb1a79a165fb20a
NPNarayan <nishiprakashnarayan@gmail.com>
2025-02-03 18:13:24 +0530

    030220251813

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import WebDevelopment from './pages/Services/WebDevelopment';
@@ -17,37 +16,52 @@ import Contact from './pages/More/Contact';
 import Events from './pages/More/Events';
 import HelpCenter from './pages/More/HelpCenter';
 import Security from './pages/More/Security';
-import Footer from './components/Footer';
 import NoPage from './components/NoPage';
+import Layout from './pages/Layout';
 
 const App = () => {
   return (
-    <>
-      <div className='flex flex-col min-h-screen'>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/web_development" element={<WebDevelopment />} />
-          <Route path="/app_development" element={<AppDevelopment />} />
-          <Route path="/digital_marketing" element={<DigitalMarketing />} />
-          <Route path="/software_development" element={<SoftwareDevelopment />} />
-          <Route path="/ui_ux_design" element={<UIUXDesign />} />
-          <Route path="/cctv_installation" element={<CCTVInstallation />} />
-          <Route path="/business_services" element={<BusinessServices />} />
-          <Route path="/domain_hosting" element={<DomainHosting />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/clients" element={<Clients />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/help_center" element={<HelpCenter />} />
-          <Route path="/security" element={<Security />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </>
+    <Routes>
+      {/* <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/web_development" element={<WebDevelopment />} />
+      <Route path="/app_development" element={<AppDevelopment />} />
+      <Route path="/digital_marketing" element={<DigitalMarketing />} />
+      <Route path="/software_development" element={<SoftwareDevelopment />} />
+      <Route path="/ui_ux_design" element={<UIUXDesign />} />
+      <Route path="/cctv_installation" element={<CCTVInstallation />} />
+      <Route path="/business_services" element={<BusinessServices />} />
+      <Route path="/domain_hosting" element={<DomainHosting />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/clients" element={<Clients />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/help_center" element={<HelpCenter />} />
+      <Route path="/security" element={<Security />} />
+      <Route path="*" element={<NoPage />} /> */}
+
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="web_development" element={<WebDevelopment />} />
+        <Route path="app_development" element={<AppDevelopment />} />
+        <Route path="digital_marketing" element={<DigitalMarketing />} />
+        <Route path="software_development" element={<SoftwareDevelopment />} />
+        <Route path="ui_ux_design" element={<UIUXDesign />} />
+        <Route path="cctv_installation" element={<CCTVInstallation />} />
+        <Route path="business_services" element={<BusinessServices />} />
+        <Route path="domain_hosting" element={<DomainHosting />} />
+        <Route path="products" element={<Products />} />
+        <Route path="clients" element={<Clients />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="events" element={<Events />} />
+        <Route path="help_center" element={<HelpCenter />} />
+        <Route path="security" element={<Security />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
   );
 };
 
